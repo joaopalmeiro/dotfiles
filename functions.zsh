9a40295e612711cb009cf489a03cefb3f95e37d2
João Palmeiro <joaommpalmeiro@gmail.com>
2021-02-03 13:48:54 +0000

    Update functions

--- functions.zsh
+++ functions.zsh
@@ -1,5 +1,7 @@
 function gimail() {
-    local current_email=git config user.email
-    # git config user.email $1
-    echo "$current_email"
+    local current_email=$(git config user.email)
+
+    git config user.email $1
+
+    echo "Previous email: $current_email\nNew email: $1"
 }
