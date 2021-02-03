4e623fc24c5eb6df162ce68e9bcd444f3f750f43
João Palmeiro <joaommpalmeiro@gmail.com>
2021-02-03 12:28:01 +0000

    Update exports

--- exports.zsh
+++ exports.zsh
@@ -1,4 +1,4 @@
 # To fully run PySpark locally.
 # This solved the problem of not being able to run unit tests with UDFs.
 # Source: https://stackoverflow.com/a/52230415
-# export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
