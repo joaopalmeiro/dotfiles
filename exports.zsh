f9e6770ee8c33c2dc84deefe2c2942339ddf9fcb
João Palmeiro <joaommpalmeiro@gmail.com>
2021-03-05 01:05:16 +0000

    Add language exports

--- exports.zsh
+++ exports.zsh
@@ -2,3 +2,9 @@
 # This solved the problem of not being able to run unit tests with UDFs.
 # Source: https://stackoverflow.com/a/52230415
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+# To be able to export pygal charts (or other files with CairoSVG)
+# as PNG files (on macOS, at least).
+# Source: https://github.com/Kozea/CairoSVG/issues/207
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
